2c25592201db5972445876aaac2e37125ec9bdf6
backtofayton <yucelolcay@gmail.com>
2022-03-08 19:42:04 +0200

    Create app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import './style.scss';
-import retrieveCoinData from './modules/Comments';
+import retrieveCoinData from './modules/popup';
 import url from './api';
 
 const mockComments = document.querySelector('#mock-comments');
 const mockCoin = 'bitcoin';
 
 mockComments.addEventListener('click', () => retrieveCoinData(mockCoin));
+
 console.log(url); // to prevent linter error
