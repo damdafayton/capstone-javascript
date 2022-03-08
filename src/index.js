db15114f2e432c5cd8cb8547ea25f09751d898e6
souad988 <59707859+souad988@users.noreply.github.com>
2022-03-08 16:15:14 +0100

    Create list from fetched data

--- src/index.js
+++ src/index.js
@@ -13,14 +13,31 @@ const getData=async(request)=>{
      var data=response.json()
      return data;      
 }
+
+const createCoinElement=(coin)=>{
+     return `<li><ul class="coin">
+                     <li>${coin.market_cap_rank}</li>
+                     <li class="coinIcons"><img class="coinImage" src=${coin.image} alt='coin'/><strong>${coin.name}</strong></li>
+                     <li>${coin.symbol}
+                     <button class="buyButton">Buy
+                     <i class="fas fa-shopping-basket"></i></button>
+                     </li>
+                     <li> ${coin.current_price}usd</li>
+                     <li style="color:${coin.price_change_percentage_24h>0?'green':'red'}">${coin.price_change_percentage_24h}</li>
+                     <li>${coin.circulating_supply}</li>
+                 </ul>
+              </li>`;
+}
 const addToDom=async(request,ul)=>{
     var result=await getData(request);
-    ul.innerHtml = '';
     console.log(result);
+    ul.innerHTML = ``;
+    console.log('tag element::',ul);
      result.forEach(element => {
-             ul.innerHtml +=`<li>${element.name}</li>`;
-             console.log(element.name)
+             ul.innerHTML  +=createCoinElement(element);
           }); 
 }
 
 addToDom(request,coins);
+
+
