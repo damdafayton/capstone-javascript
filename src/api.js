5caae69b25d947c1b169733b2266282771fde5bf
souad988 <59707859+souad988@users.noreply.github.com>
2022-03-08 17:24:18 +0100

    Fix linter errors

--- src/api.js
+++
@@ -1,5 +0,0 @@
-import Request from "./modules/request";
-
-const coinsUrl='https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
-
-const request = new Request(url);
--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
 import './style.scss';
-import Request from "./modules/request";
+import Request from './modules/request';
 
-const coinsUrl='https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
-const url='https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/QzuepOdb4S7f14QxmbA0/scores';
+const coinsUrl = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
 const request = new Request(coinsUrl);
-const req= new Request(url);
-const coins=document.querySelector('.coins');
 
-const getData=async(request)=>{
-     var response= await request.get();
-    // console.log(response)
-     var data=response.json()
-     return data;      
-}
+const coins = document.querySelector('.coins');
 
-const createCoinElement=(coin)=>{
-     return `<li><ul class="coin">
+const getData = async (request) => {
+  const response = await request.get();
+  // console.log(response)
+  const data = response.json();
+  return data;
+};
+
+const createCoinElement = (coin) => `<li><ul class="coin">
                      <li>${coin.market_cap_rank}</li>
                      <li class="coinIcons"><img class="coinImage" src=${coin.image} alt='coin'/><strong>${coin.name}</strong></li>
                      <li>${coin.symbol}
@@ -23,22 +21,18 @@ const createCoinElement=(coin)=>{
                      <i class="fas fa-shopping-basket"></i></button>
                      </li>
                      <li> ${coin.current_price}$</li>
-                     <li style="color:${coin.price_change_percentage_24h>0?'green':'red'}">${coin.price_change_percentage_24h}</li>
+                     <li style="color:${coin.price_change_percentage_24h > 0 ? 'green' : 'red'}">${coin.price_change_percentage_24h}</li>
                      <li>${coin.circulating_supply}</li>
                  </ul>
               </li>`;
-}
-const addToDom=async(request,ul)=>{
-    var result=await getData(request);
-    console.log(result);
-    ul.innerHTML = ``;
-    console.log('tag element::',ul);
-    for(var i=0;i<10;i++){
-          ul.innerHTML  +=createCoinElement(result[i]);
-    }
-     
-}
+const addToDom = async (request, ul) => {
+  const result = await getData(request);
+  console.log(result);
+  ul.innerHTML = '';
+  console.log('tag element::', ul);
+  for (let i = 0; i < 10; i += 1) {
+    ul.innerHTML += createCoinElement(result[i]);
+  }
+};
 
-addToDom(request,coins);
-
-
+addToDom(request, coins);
