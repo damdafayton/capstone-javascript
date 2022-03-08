abe9c263ea6090b2a748f8344bcc4c9b6c60c8d1
souad988 <59707859+souad988@users.noreply.github.com>
2022-03-08 11:25:31 +0100

    Add data to the dom

--- src/api.js
+++ src/api.js
@@ -1,2 +1,5 @@
-var url='https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
+import Request from "./modules/request";
 
+const coinsUrl='https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
+
+const request = new Request(url);
--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import './style.scss';
+import Request from "./modules/request";
+
+const coinsUrl='https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
+const url='https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/QzuepOdb4S7f14QxmbA0/scores';
+const request = new Request(coinsUrl);
+const req= new Request(url);
+const coins=document.querySelector('.coins');
+
+const getData=async(request)=>{
+     var response= await request.get();
+    // console.log(response)
+     var data=response.json()
+     return data;      
+}
+const addToDom=async(request,ul)=>{
+    var result=await getData(request);
+    ul.innerHtml = '';
+    console.log(result);
+     result.forEach(element => {
+             ul.innerHtml +=`<li>${element.name}</li>`;
+             console.log(element.name)
+          }); 
+}
+
+addToDom(request,coins);
