246aed6d3b33a86da126e859c1608784007d1f58
backtofayton <yucelolcay@gmail.com>
2022-03-08 19:42:04 +0200

    Create app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './style.scss';
-import { addToDom , cryptoCount, getData} from './modules/Home';
+import { addToDom, cryptoCount, getData } from './modules/Home';
 import { request } from './modules/api';
 
 const coins = document.querySelector('.coins');
